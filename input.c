eb153218ce6ccf9fbf54b3c11a919d46ad2fd420
Razist <squeezling@hotmail.com>
2020-11-06 13:11:04 +0800

    Add files via upload

--- input.c
+++ input.c
@@ -1,35 +1,79 @@
 #include<stdio.h>
-void inputData(char *filename);
+int inputData(char *filename);
+int traverseArray(int m,int n,float array[m][n]);
 int main(){
-    char xd[]="fertility_Diagnosis_Data_Group9_13.txt";
-    inputData(xd);
+    char filename[]="fertility_Diagnosis_Data_Group9_13.txt";
+    inputData(filename);
     return 0;
 }
-void inputData(char *filename){
+int inputData(char *filename){
+    //creation of arrays(hardcoded)
     float inputarray[100][10];
+    float inputtrain[90][9];
+    float inputtest[10][9];
+    float outputtrain[90][1];
+    float outputtest[10][1];
     FILE *fertilitydata;
-    if(fertilitydata==NULL){
+    if(fertilitydata==NULL){//if file is unable to be found
         printf("Error while opening file!\n");
     }
-    else{
+    else{//if file is found
         printf("File found successfully!\n");
-        fertilitydata = fopen(filename,"r");
+        fertilitydata = fopen(filename,"r");//open file in read only
+        //input data in array
         for(int i=0;i<100;i++){
             for(int j=0;j<10;j++){
                 fscanf(fertilitydata,"%f%*c",&inputarray[i][j]);
                 //printf("%f",inputarray[i][j]);
             }
         }
-        fclose(fertilitydata);
+        fclose(fertilitydata);//close access to file
 
-        //Traverse 2D array
+        //Splitting of data into 4 different sets
         for(int i=0;i<100;i++){
-                printf("Array #%d\n",i+1);
+            if(i<90){
                 for(int j=0;j<10;j++){
-                    printf("Element #%d is %f \n",j+1,inputarray[i][j]);
+                    if(j<9){
+                        inputtrain[i][j]=inputarray[i][j];
+                    }
+                    else{
+                        outputtrain[i][0]=inputarray[i][j];
+                    }
                 }
-                printf("\n");
             }
+            else{
+                for(int j=0;j<10;j++){
+                    if(j<9){
+                        //printf("Array %d: %.2f\n",i+1,inputarray[i][j]);
+                        inputtest[i-90][j]=inputarray[i][j];
+                    }
+                    else{
+                        //printf("Array %d: %.2f\n",i+1,inputarray[i][j]);
+                        outputtest[i-90][0]=inputarray[i][j];
+                    }
+                }
+            }
+        }
+
+        printf("Traverse Input Training array:\n");
+        traverseArray(90,9,inputtrain);
+        printf("Traverse Output Training array:\n");
+        traverseArray(90,1,outputtrain);
+        printf("Traverse Input Test array:\n");
+        traverseArray(10,9,inputtest);
+        printf("Traverse Output Test array:\n");
+        traverseArray(10,1,outputtest);
+
     }
     return 1;
+}
+int traverseArray(int m,int n,float array[m][n]){
+    for(int i=0;i<m;i++){
+            printf("Array %d: ",i+1);
+            for(int j=0;j<n;j++){
+                printf("%.2f ",array[i][j]);
+            }
+            printf("\n");
+        }
+    return 2;
 }
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include"input.h"
 int main(){
+    int numoftrain=50, numoftest=50;//In future can read in user input :D
     char filename[]="fertility_Diagnosis_Data_Group9_13.txt";
-    inputData(filename);//call function to read in data set input into array
+    inputData(numoftrain,numoftest,filename);//call function to read in data set input into array and traverse it
     return 0;
 }
