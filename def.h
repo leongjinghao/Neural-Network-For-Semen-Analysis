4815b40aed423b641afae317aeee5ef72de8e885
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2020-11-18 23:15:32 +0800

    -

---
+++ def.h
@@ -0,0 +1,7 @@
+#define FILENAME "fertility_Diagnosis_Data_Group9_13.txt"
+#define FILEOUT "mae_Output.txt"
+#define trainRow 90
+#define testRow 10
+#define col 9
+#define targetMAE 0.1
+#define mu 0.05
